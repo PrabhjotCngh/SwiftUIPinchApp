132c3fbe19f0d529fc0ae3d93c59e801ffcb9dc6
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-05-18 15:18:51 -0400

    First commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by M_2195552 on 2023-05-18.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
